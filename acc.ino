f85b3ca729ebcb62ec6adc8d351249504b5f7095
lokesh-8837 <141553551+lokesh-8837@users.noreply.github.com>
2024-05-09 17:05:36 +0530

    Add files via upload

---
+++ acc.ino
@@ -0,0 +1,39 @@
+#include <AFMotor.h>
+#include <ros.h>
+#include <std_msgs/Int16.h> // Include the message type for speed command
+
+AF_DCMotor motor1(1); // Motor 1 is connected to M1 port
+AF_DCMotor motor2(2); // Motor 2 is connected to M2 port
+AF_DCMotor motor3(3); // Motor 3 is connected to M3 port
+AF_DCMotor motor4(4); // Motor 4 is connected to M4 port
+
+ros::NodeHandle nh;
+
+void commandCallback(const std_msgs::Int16& msg) {
+  int speed = msg.data;
+  moveForward(speed);
+}
+
+ros::Subscriber<std_msgs::Int16> sub("/robot_speed", &commandCallback);
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+void loop() {
+  nh.spinOnce();
+}
+
+void moveForward(int speed) {
+  motor1.setSpeed(speed);
+  motor1.run(FORWARD);
+  motor2.setSpeed(speed);
+  motor2.run(FORWARD);
+  motor3.setSpeed(speed);
+  motor3.run(FORWARD);
+  motor4.setSpeed(speed);
+  motor4.run(FORWARD);
+
+}
---
+++ ultra_adap_03.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32, String
+import serial
+
+# Global variables
+pub_robot_commands = None
+desired_distance = 20  # Desired distance between the bots in cm
+prev_error = 0  # Previous error for integral term
+dict_constants = {(-20,0):[0,0.5],(0,20):[2,0.5],(20,40):[4,1],(40,60):[6,2],(60,1999):[10,5]}
+
+def get_values(error, data):
+    for key in data:
+        if key[0] <= error <= key[1]:
+            Kp, Ki = data[key]
+            return Kp, Ki
+    return None
+#print(get_values(30,dict_constants))
+
+# Define callback function for distance_sensor1 subscriber
+
+def sensor1_callback(data):
+    global pub_robot_commands, prev_error
+    
+    # Calculate error (difference between desired distance and actual distance)
+    error = data.data -desired_distance 
+    
+    # Integral term
+    integral = prev_error + error
+    Kp = get_values(error, dict_constants)[0]
+    Ki = get_values(error, dict_constants)[1]
+    
+  
+    
+    
+    
+    # Calculate speed based on proportional and integral control
+    speed = int(Kp * error + Ki * integral)
+
+    speed = min(max(speed, 0), 255)  # Limit speed to range [0, 255]
+    rospy.loginfo("Distance %d , Error: %d Kp: %d , Ki: %d , Speed: %d ,Prev_error %d "  % (data.data ,error, Kp ,Ki,speed,prev_error))
+    # Publish speed command
+    pub_robot_commands.publish(str(speed))
+    
+    # Update previous error
+    prev_error = error
+    
+
+def sensor_logic():
+    global pub_robot_commands
+    rospy.init_node('sensor_logic', anonymous=True)
+    
+    # Subscribe to distance_sensor1 topic
+    rospy.Subscriber('distance_sensor2', Float32, sensor1_callback)
+
+    # Initialize serial connection
+    # ser = serial.Serial('/dev/ttyUSB0', 9600)  # Modify port and baudrate as needed
+    
+    # Publish robot speed commands
+    pub_robot_commands = rospy.Publisher('/robot_speed', String, queue_size=10)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    sensor_logic()
